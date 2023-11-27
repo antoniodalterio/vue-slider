3bcbe87ad17ac208ed8b925a8a89eda522fda199
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-27 21:07:25 +0100

    feat: al click cambia img

--- js/script.js
+++ js/script.js
@@ -31,6 +31,16 @@ createApp({
           text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
         },
       ],
+      currentIndex: 0,
     };
+  },
+  methods: {
+    nextImg(index) {
+      this.currentIndex++;
+    },
+
+    prevImg(index) {
+      this.currentIndex--;
+    },
   },
 }).mount('#app');
